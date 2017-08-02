5aac85cf6f3db19911f0b8d539f6fef09c3da665
JanSchoenfeld <jan.schoenfeld@fh-muenster.de>
2017-08-02 11:18:04 +0200

    Add functionality to open files TODO: keep formatting

--- Hello.py
+++ Hello.py
@@ -1,4 +1,5 @@
 import os
+import io
 from flask import Flask, url_for, request, render_template
 from flask_bootstrap import Bootstrap
 
@@ -17,7 +18,7 @@ def start():
 
 @app.route("/", methods = ["POST", "GET"])
 def browse_path():
-    filepath = request.form['filepath']
+    filepath = request.form['path']
     if filepath == "":
         os.chdir("..")
         cwd = os.getcwd()
@@ -34,6 +35,15 @@ def content():
     directories = next(os.walk(cwd))[1]
     directories_uni = [i.decode('UTF-8') if isinstance(i, basestring) else i for i in directories]
     return render_template("index_files.html", cwd = cwd, files = files_uni, directories = directories_uni)
+
+@app.route("/open", methods = ["POST", "GET"])
+def read_file():
+    filename = request.form['filepath']
+    cwd = os.getcwd()
+    file_data = io.open(cwd + '/' + filename, 'r', encoding = 'utf-8').readlines()
+    file_data = [line.strip() for line in file_data]
+    return render_template("index_open.html", open_file = file_data, cwd = cwd, filename = filename)
+
 
 if __name__ == "__main__":
     app.run(debug = True)
