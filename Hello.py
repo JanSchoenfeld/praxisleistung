ad2583994947094797d2c5715acc2dfa6ed7b65a
JanSchoenfeld <jan.schoenfeld@fh-muenster.de>
2017-08-02 11:47:23 +0200

    add comments

--- Hello.py
+++ Hello.py
@@ -1,21 +1,21 @@
 import os
 import io
 from flask import Flask, url_for, request, render_template
-from flask_bootstrap import Bootstrap
 
 def create_app():
     app = Flask(__name__)
-    Bootstrap(app)
     return app
 
 app = create_app()
 
+# Root URL, 127.0.0.1:5000/
+# Offers interface to change working directory and view files.
 @app.route("/")
 def start():
     cwd = os.getcwd()
     return render_template("index.html", cwd = cwd)
 
-
+# Method to change the current working directory if specified, or go up into the parent directory
 @app.route("/", methods = ["POST", "GET"])
 def browse_path():
     filepath = request.form['path']
@@ -27,6 +27,7 @@ def browse_path():
         cwd = os.getcwd()
     return render_template("index.html", cwd = cwd)
 
+#Method to list the content (directories, files) of the specified path using an extended index.html
 @app.route("/content")
 def content():
     cwd = os.getcwd()
@@ -36,6 +37,8 @@ def content():
     directories_unicode = [i.decode('UTF-8') if isinstance(i, basestring) else i for i in directories]
     return render_template("index_files.html", cwd = cwd, files = files_unicode, directories = directories_unicode)
 
+#Method to open a file and show its content on an extended index.html
+#TODO: Keep file formatting for easier readability
 @app.route("/open", methods = ["POST", "GET"])
 def read_file():
     filename = request.form['filepath']
